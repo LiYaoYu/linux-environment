5827fe23672ab86c631dd957028d6e6eb2dfe6e9
Yao-Yu Li <forever3580ya@gmail.com>
2018-05-26 17:27:08 +0800

    Refine install.sh

--- install.sh
+++ install.sh
@@ -6,35 +6,26 @@ install_requirements() {
 }
 
 
-copy_required_files() {
-  ln -fs `pwd`/vim/vimrc ~/.vimrc
-  ln -fs `pwd`/vim/ycm_extra_conf.py ~/.ycm_extra_conf.py
-  ln -fs `pwd`/tmux/tmux.conf ~/.tmux.conf
-}
-
-
 replace_rm() {
   mv /bin/rm bin/rm.bak
   ln -fs `pwd`/rm/rm.rep /bin/rm
 }
 
 
-install_vundle() {
+install_and_set_vim() {
+  ln -fs `pwd`/vim/vimrc ~/.vimrc
+  ln -fs `pwd`/vim/ycm_extra_conf.py ~/.ycm_extra_conf.py
+
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-}
-
-
-install_vim_plugins() {
   vim +PluginInstall +qall
-}
-
-
-compile_ycm_essential() {
   ~/.vim/bundle/YouCompleteMe/install.sh --clang-completer
 }
 
 
 install_and_set_ohmyzsh() {
+  ln -fs `pwd`/zsh/zshrc ~/.zshrc
+  ln -fs `pwd`/zsh/mytheme.zsh-theme ~/.oh-my-zsh/themes/mytheme.zsh-theme
+
   # "https://git.io/vhqYi" is the shorten URL of oh-my-zsh installation script
   # which allows batch mode and is updated in pull requests #5893, link is as 
   # below:
@@ -43,36 +34,29 @@ install_and_set_ohmyzsh() {
     echo "Could not install Oh My Zsh" >/dev/stderr
     exit 1
   }
-
-  ln -fs `pwd`/zsh/zshrc ~/.zshrc
-  ln -fs `pwd`/zsh/mytheme.zsh-theme ~/.oh-my-zsh/themes/mytheme.zsh-theme
 }
 
 
-main () {
+install_and_set_tmux() {
+  git clone https://github.com/tmux-plugins/tmux-resurrect `pwd`/tmux/tres
+  ln -fs `pwd`/tmux/tmux.conf ~/.tmux.conf
+}
+
+main() {
   echo installing requirements ...
   install_requirements
-
-  echo copying required files to home ...
-  copy_required_files
 
   # echo replace rm command ...
   #replace_rm
 
-  echo installing vundle ...
-  install_vundle
-
-  echo installing vim plugins ...
-  install_vim_plugins
-
-  echo compiling YCM with semantic support for C-family languages ...
-  compile_ycm_essential
+  echo installing and setting vim ...
+  install_and_set_vim
 
   echo installing and setting oh-my-zsh ...
   install_and_set_ohmyzsh
 
-  echo running oh-my-zsh with settings
-  env zsh
+  echo installing and setting tmux ...
+  install_and_set_tmux
 }
 
 main
