c2c371a0ea61b6e25f9aa5832ecf5e2037968993
Yao-Yu Li <forever3580ya@gmail.com>
2018-05-26 16:27:32 +0800

    Enable install.sh to install zsh in batch mode

---
+++ install.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+install_requirements() {
+  sudo apt-get update
+  sudo apt install cscope cmake python-dev python3-dev tmux zsh build-essential
+}
+
+
+copy_required_files() {
+  ln -fs `pwd`/vim/vimrc ~/.vimrc
+  ln -fs `pwd`/vim/ycm_extra_conf.py ~/.ycm_extra_conf.py
+  ln -fs `pwd`/tmux/tmux.conf ~/.tmux.conf
+}
+
+
+replace_rm() {
+  mv /bin/rm bin/rm.bak
+  ln -fs `pwd`/rm/rm.rep /bin/rm
+}
+
+
+install_vundle() {
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+}
+
+
+install_vim_plugins() {
+  vim +PluginInstall +qall
+}
+
+
+compile_ycm_essential() {
+  ~/.vim/bundle/YouCompleteMe/install.sh --clang-completer
+}
+
+
+install_ohmyzsh() {
+  # "https://git.io/vhqYi" is the shorten URL of oh-my-zsh installation script
+  # which allows batch mode and is updated in pull requests #5893, link is as 
+  # below:
+  # "https://github.com/robbyrussell/oh-my-zsh/pull/5893"
+  sh -c "$(curl -fsSL https://git.io/vhqYi)" -s --batch || {
+    echo "Could not install Oh My Zsh" >/dev/stderr
+    exit 1
+  }
+}
+
+
+main () {
+  echo installing requirements ...
+  install_requirements
+
+  echo copying required files to home ...
+  copy_required_files
+
+  # echo replace rm command ...
+  #replace_rm
+
+  echo installing vundle ...
+  install_vundle
+
+  echo installing vim plugins ...
+  install_vim_plugins
+
+  echo compiling YCM with semantic support for C-family languages ...
+  compile_ycm_essential
+
+  echo installing oh-my-zsh ...
+  install_ohmyzsh
+}
+
+main
